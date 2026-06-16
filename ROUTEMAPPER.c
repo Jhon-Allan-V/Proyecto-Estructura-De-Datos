6388607bb25f43c25fc96188a7e8ffbfde725312
Jhon-Allan-V <jhon.veliz.h@mail.pucv.cl>
2026-06-15 23:15:55 -0400

    se intenta dejar en claro de como deberia quedar la (forma) del programa

--- ROUTEMAPPER.c
+++ ROUTEMAPPER.c
@@ -50,6 +50,7 @@ typedef struct {
     char archivoRutas[MAX];
 } State;
 
+/*
 typedef struct {
     int origen;
     int destino;
@@ -58,10 +59,13 @@ typedef struct {
 
     char calle[MAX];
 } accion;
+ */
 
+ /*
 int distancia_L1(State* state) {
     return abs(state->x - (N-1)) + abs(state->y - (N-1));
 }
+*/
 
 int esSolucionFinal(State *actual){
     return actual -> x == N-1 && actual -> y == N-1;
@@ -81,110 +85,18 @@ int heapEstaVacia(Heap *heap){
 
 // Función para imprimir el estado actual
 void imprimirEstado(const State *estado) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (estado->x==i && estado->y==j) printf(" A ");
-            else if (i == 0 && j == 0) printf(" I ");
-            else if (i == N-1 && j == N-1) printf(" M ");
-            else if (estado->maze[i][j] == 0)
-                printf(" . "); // Imprime un espacio en blanco para el espacio vacío
-            else
-                printf("[X]");
-        }
-        printf("\n");
-    }
 }
 
-void imprimirCamino(State *camino){
-    accion *movimiento = list_first(camino -> actions);
-      /*{-1,0},//arriba
-        {1,0},//abajo
-        {0,-1},//izq
-        {0,1}//der*/
-    printf("\nPasos totales a realizar -> %i\n", camino -> steps);
-
-    while (movimiento != NULL){
-        if (movimiento -> accionX == -1 && movimiento -> accionY == 0)printf("Arriba\n");
-        if (movimiento -> accionX == 1 && movimiento -> accionY == 0)printf("Abajo\n");
-        if (movimiento -> accionX == 0 && movimiento -> accionY == -1)printf("Izquierda\n");
-        if (movimiento -> accionX == 0 && movimiento -> accionY == 1)printf("Derecha\n");
-
-        movimiento = list_next(camino -> actions);
-    }
+void imprimirCamino(const State *estado){
 }
 
-State crearEstadoInicial(int maze[N][N], int dificultad){
-    State estado;
-     // Copiar el laberinto generado al estado
-    generate_maze(estado.maze,  dificultad);
-    estado.x = 0;
-    estado.y = 0;
-    estado.steps = 0;
-    estado.actions = list_create();
-    return estado;
+State *crearEstadoInicial(){
 }
 
-State *transition(State *actual, accion movimiento){
-    State *nuevo = malloc(sizeof(State));
-    if (nuevo == NULL) exit(EXIT_FAILURE);
-
-    *nuevo = *actual;
-
-    nuevo -> x += movimiento.accionX;
-    nuevo -> y += movimiento.accionY;
-    nuevo -> steps += 1;//actual -> steps +1;
-    nuevo -> actions = list_create();
-
-    accion *aux = list_first(actual -> actions);
-
-    while (aux != NULL){
-        accion *copia = malloc(sizeof(accion));
-        if (copia == NULL) exit(EXIT_FAILURE);
-
-        *copia = *aux;
-
-        list_pushBack(nuevo -> actions, copia);
-
-        aux = list_next(actual -> actions);
-    }
-
-    accion *nuevaAccion = malloc(sizeof(accion));
-    if (nuevaAccion == NULL) exit(EXIT_FAILURE);
-
-    *nuevaAccion = movimiento;
-
-    list_pushBack(nuevo -> actions, nuevaAccion);
-
-    return nuevo;
+State *transition(State *actual){
 }
 
 List *getAdjNodes(State *actual){
-    List *vecinos = list_create();
-
-    accion acciones[] = {
-        {-1,0},//arriba
-        {1,0},//abajo
-        {0,-1},//izq
-        {0,1}//der
-    };
-
-    for (unsigned short i = 0; i < 4; i++){
-        accion movimiento = acciones[i];
-
-        int nuevaFila = actual -> x + movimiento.accionX;
-        int nuevaCol = actual -> y + movimiento.accionY;
-
-        //verificar si se sale de los limites
-        if (nuevaFila < 0 || nuevaFila >= N ||
-            nuevaCol < 0 || nuevaCol >= N) continue;
-
-        //verificar si estamos en un obstaculo
-        if (actual -> maze[nuevaFila][nuevaCol] == 1) continue;
-
-        State *nuevo = transition(actual, movimiento);
-        list_pushBack(vecinos, nuevo);
-    }
-    return vecinos;
 }
 
 void dfs(State *estado_inicial){
@@ -304,7 +216,7 @@ int main() {
     char opcion;
     do {
 
-        printf("\n\n***** ValSearchpo ******\n");
+        printf("\n\n***** ROUTERMAPPER ******\n");
         puts("========================================");
         puts("        Escoge alguna opcion            ");
         puts("========================================");

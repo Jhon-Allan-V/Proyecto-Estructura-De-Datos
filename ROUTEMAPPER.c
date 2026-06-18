ab17038640812c6659368fcf7da1af1c268244f6
Jhon-Allan-V <jhon.veliz.h@mail.pucv.cl>
2026-06-18 06:31:36 -0400

    se logran mostrar informacion de 100 filas de la DB

--- ROUTEMAPPER.c
+++ ROUTEMAPPER.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 #include "tdas/list.h"
+#include "tdas/map.h"
 #include "tdas/stack.h"
 #include "tdas/queue.h"
 #include "tdas/heap.h"
@@ -11,6 +13,11 @@
 
 #define MAX 256
 
+//se usara para almaxenar una key temporal, para validar que no se repitan calles en el grafo
+typedef struct{
+    double keyLongitud;
+    double keyLattitud;
+}keyParaMapaTemporal;
 
 typedef struct {
     int id;
@@ -45,11 +52,12 @@ typedef struct {
     int cantidadAristas; //cantidad de rutas
 } Grafo;
 
-Grafo *generarGrafo() {
+Grafo *generarGrafo(){
     Grafo *grafo = malloc(sizeof(Grafo));
-    grafo->vertices = NULL;
-    grafo->cantidadVertices = 0;
-    grafo->cantidadAristas = 0;
+    if (!grafo) return NULL;
+    grafo -> vertices = list_create();
+    grafo -> cantidadVertices = 0;
+    grafo -> cantidadAristas = 0;
 
     return grafo;
 }
@@ -74,16 +82,22 @@ void consultarTablasDB(sqlite3 *db){
         printf("ERROR: %s\n", error);
         sqlite3_free(error);
     }
+}
 
-    /*if (error != NULL){
-        printf("ERROR: %s\n", error);
-        sqlite3_free(error);
-    }*/
+void cargarDatosAlGrafo(Grafo *grafo, const char *nombre,
+                               const char *clase, const char *oneway,
+                               double lon1, double lat1, double lon2, double lat2) {
+
 }
 
 //primero se verifica si la base de datos puede ser abrida o leida por el programa
-void cargarDatosAlGrafo(Grafo *grafo) {
-    if (grafo -> vertices != NULL) {
+void obtenerInformacionDB(Grafo *grafo){
+    if (grafo == NULL) {
+        printf("Error: grafo no inicializado.\n");
+        return;
+    }
+
+    if (grafo->vertices != NULL && list_size(grafo->vertices) > 0) {
         printf("El grafo ya tiene datos cargados. No se pueden cargar nuevamente.\n");
         return;
     }
@@ -104,11 +118,11 @@ void cargarDatosAlGrafo(Grafo *grafo) {
     //consultarTablasDB(db); //mostrar las tablas de la base de datos para verificar lectura de este
 
     sqlite3_stmt *stmt;
-    //cargar hasta 100 primeras filas de la tabla gis_osm_roads_free, que contiene las calles y caminos de chile
+    // leer todos los segmentos de la tabla gis_osm_roads_free, que contiene las calles y caminos de chile
     const char *comando = "SELECT fid, geom, name, fclass, oneway FROM gis_osm_roads_free LIMIT 100;";
     rc = sqlite3_prepare_v2(db, comando, -1, &stmt, NULL);
 
-    if (rc != SQLITE_OK) {
+    if (rc != SQLITE_OK){
         printf("ERROR: %s\n", sqlite3_errmsg(db));
 
         presioneTeclaParaContinuar();
@@ -121,24 +135,28 @@ void cargarDatosAlGrafo(Grafo *grafo) {
 
     //se iteran las filas
     while (sqlite3_step(stmt) == SQLITE_ROW){
-        //extraer datos
         int fid = sqlite3_column_int(stmt, 0);
         const unsigned char *geom = sqlite3_column_blob(stmt, 1); //dato binario
         int geom_size = sqlite3_column_bytes(stmt, 1);
-        const char *nombre = sqlite3_column_text(stmt, 2);
-        const char *clase = sqlite3_column_text(stmt, 3);
-        const char *oneway = sqlite3_column_text(stmt, 4);
+        const char *nombre = (const char *)sqlite3_column_text(stmt, 2); //nombre de la calle
+        const char *clase = (const char *)sqlite3_column_text(stmt, 3); //tipo de calle (residential, primary, secondary, etc)
+        const char *oneway = (const char *)sqlite3_column_text(stmt, 4); //si es un solo sentido o no
 
         double lon1, lat1, lon2, lat2;
         int resultadoParseo = parse_gpkg_linestring(geom, geom_size, &lon1, &lat1, &lon2, &lat2);
+
         if (resultadoParseo != 1){
-            printf("Error al parsear la geometria: %s\n", geom);
+            printf("Error al parsear la geometria en fila %d.\n", fid);
             continue; //se salta esta fila y se sigue con la siguiente
         }
+
+        printf("\nFila %d: %s, %s, %s, (%lf, %lf) -> (%lf, %lf)\n", fid, nombre, clase, oneway, lon1, lat1, lon2, lat2);
+
+        //crear mapa temporal (tabla hash) para verificar que no se repitan calles, la key seria : (lon, lat)
+        //Map *mapaTemporal = sorted_map_create(compararKeysMapaTemporal););
+        //cargarDatosAlGrafo(grafo, nombre, clase, oneway, lon1, lat1, lon2, lat2);
     }
 
-    
-    //una vez ya cargados los datos al grafo, se cierra la base da datos
     sqlite3_finalize(stmt);
     sqlite3_close(db);
 }
@@ -213,7 +231,7 @@ int main() {
 
         switch (opcion) {
         case '1':
-            cargarDatosAlGrafo(grafo); // se lee la base de datos y se llena el grafo con los lugares y conexiones
+            obtenerInformacionDB(grafo); // se lee la base de datos y se llena el grafo con los lugares y conexiones
             break;
         case '2':
             configuracionInicial();
